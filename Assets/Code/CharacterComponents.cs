5e5896577c8d3e1b8f096b2d55072b3b30d221e8
Robin <robin@rvanee.nl>
2023-01-22 13:31:57 +0100

    Example setup

---
+++ Assets/Code/CharacterComponents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using WolfRPG.Core;
+
+namespace Code
+{
+	public class CharacterBaseProperties: IRPGComponent
+	{
+		public string CharacterName { get; set; }
+		[AssetReference(typeof(Sprite))]
+		public AssetReference Portrait { get; set; }
+	}
+
+	public class ExampleComponent : IRPGComponent
+	{
+		[AssetReference(typeof(Sprite))]
+		public AssetReference MySprite { get; set; }
+		
+		[AssetReference(typeof(GameObject))]
+		public AssetReference MyPrefab { get; set; }
+	}
+
+	public class CharacterProgression : IRPGComponent
+	{
+		public int BaseLevel { get; set; }
+		public AnimationCurve XPCurve { get; set; }
+	}
+}
---
+++ Assets/Code/TestMonoBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using WolfRPG.Core;
+
+namespace Code
+{
+	public class TestMonoBehaviour: MonoBehaviour
+	{
+		[SerializeField] private RPGObjectReference characterReference;
+
+		private void Awake()
+		{
+			// Load the RPGObject from the reference
+			// The first time the asset is loaded from disk and deserialized, but it's cached afterwards
+			IRPGObject character = characterReference.GetObject();
+			
+			// Use GetComponent to retrieve our data from the RPGObject
+			ExampleComponent component = character.GetComponent<ExampleComponent>();
+
+			// Unity assets are referenced with the AssetReference type
+			AssetReference prefabReference = component.MyPrefab;
+
+			// Retrieve the prefab from our prefab reference
+			// The first time this loads the asset from the disk, but is cached in memory afterwards
+			GameObject prefab = prefabReference.GetAsset<GameObject>();
+			
+			// Instantiate a GameObject from our prefab, using the normal Unity method, add a sprite object
+			GameObject myGameObject = Instantiate(prefab);
+
+			// Load the Sprite using the same method as before with the prefab
+			AssetReference spriteReference = component.MySprite;
+			Sprite sprite = spriteReference.GetAsset<Sprite>();
+			
+			// Add a spriteRenderer so we can render our sprite
+			SpriteRenderer spriteRenderer = myGameObject.AddComponent<SpriteRenderer>();
+			spriteRenderer.sprite = sprite;
+		}
+	}
+}
