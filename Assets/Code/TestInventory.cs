891b2522f190fbf516eb2592084cdb8090f28310
Robin <robin@rvanee.nl>
2023-03-19 17:54:18 +0100

    Inventory stuff

---
+++ Assets/Code/TestInventory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using WolfRPG.Core;
+using WolfRPG.Inventory;
+
+namespace Code
+{
+	public class TestInventory:  MonoBehaviour
+	{
+		[SerializeField] private RPGObjectReference[] items;
+		[SerializeField] private InventoryPanelController inventoryPanel;
+
+		private void Start()
+		{
+			var container = new ItemContainer();
+			foreach (var item in items)
+			{
+				container.AddItem(item.GetObject());
+			}
+			
+			inventoryPanel.Show(container);
+		}
+	}
+}
